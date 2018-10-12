fb681cd1c2f510e0c314dec3c0deb9e84f62c08c
Kllami <geotarsis@gmail.com>
2018-10-11 18:47:12 -0600

    Modulo para añadir perfiles agregado y agregado acceso en el login.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AgregarDatosComponent} from './agregar-datos/agregar-datos.component';
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './login/login.component';
+import {AgregarPerfilesComponent} from './agregar-perfiles/agregar-perfiles.component';
 
 /* El primer path, cuyas comillas estan vacias, debe ser asi para indicar que es la ventana default */
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'datos', component: AgregarDatosComponent}
-
+  {path: 'datos', component: AgregarDatosComponent},
+  {path: 'añadirPerfiles', component: AgregarPerfilesComponent}
 ];
 
 @NgModule({
